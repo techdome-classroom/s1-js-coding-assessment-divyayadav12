3fb40ca89f85179fcb32d111edcd9430b08441ba
Divya yadav <divyayadav141203@gmail.com>
2024-09-25 13:20:20 +0530

    Your commit message

--- program1.js
+++ program1.js
@@ -1,8 +1,37 @@
 const getTotalIsles = function (grid) {
+  if (!grid || grid.length === 0) return 0;
 
+  const rows = grid.length;
+  const cols = grid[0].length;
+  let islandCount = 0;
 
-  // write your code here
+  const dfs = (r, c) => {
+      
+      if (r < 0 || r >= rows || c < 0 || c >= cols || grid[r][c] === 'W') {
+          return;
+      }
 
+       
+      grid[r][c] = 'W';
+
+      
+      dfs(r + 1, c);  
+      dfs(r - 1, c);  
+      dfs(r, c + 1);  
+      dfs(r, c - 1);  
+  };
+
+   
+  for (let r = 0; r < rows; r++) {
+      for (let c = 0; c < cols; c++) {
+          if (grid[r][c] === 'L') {
+              islandCount++;
+              dfs(r, c);   
+          }
+      }
+  }
+
+  return islandCount;
 };
 
 module.exports = getTotalIsles;
--- program2.js
+++ program2.js
@@ -1,7 +1,28 @@
 const decodeTheRing = function (s, p) {
+  const isMatch = (i, j) => {
+      
+      if (i >= s.length && j >= p.length) return true;
 
-    // write your code here
+      
+      if (j >= p.length) return false;
 
+      
+      if (p[j] === '*') {
+           
+          if (isMatch(i, j + 1)) return true; 
+          if (i < s.length && isMatch(i + 1, j)) return true; 
+      }
+
+       
+      if (i < s.length && (p[j] === '?' || p[j] === s[i])) {
+          return isMatch(i + 1, j + 1);
+      }
+
+      
+      return false;
   };
-  
-  module.exports = decodeTheRing;
+
+  return isMatch(0, 0);
+};
+
+module.exports = decodeTheRing;
